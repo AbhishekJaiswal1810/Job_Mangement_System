4024daaaf9ef0facadeac8fe63f5f162f4c1d6a4
burhan-j <114441687+burhan-j@users.noreply.github.com>
2022-12-08 18:37:49 -0500

    Drop operations

---
+++ Burhanuddin-002650009/20-dropoperations.sql
@@ -0,0 +1,36 @@
+drop table job_applications;
+drop table job_skill;
+drop table job_posting;
+drop table recruiter;
+drop table company;
+drop table applicant_skill;
+drop table applicant_profile;
+drop table skill_set;
+drop table user_account;
+drop table user_type;
+
+
+drop sequence user_type_sequence;
+drop sequence skill_set_sequence;
+drop sequence applicant_profile_sequence;
+drop sequence company_sequence;
+drop sequence recruiter_sequence;
+drop sequence job_posting_sequence;
+drop sequence applicant_skill_sequence;
+drop sequence job_skill_sequence;
+drop sequence job_applications_sequence;
+
+truncate table job_applications;
+truncate table job_skill;
+truncate table job_posting;
+truncate table recruiter;
+truncate table company;
+truncate table applicant_skill;
+truncate table applicant_profile;
+truncate table skill_set;
+truncate table user_account;
+truncate table user_type;
+
+
+
+
